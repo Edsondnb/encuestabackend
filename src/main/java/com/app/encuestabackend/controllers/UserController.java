7cba400b49e6cc95261dbcba0300e8249cb1b5f4
eds <n3.edson@hotmail.com>
2024-05-10 10:24:40 -0500

    Avance UserTokkenPasswordAndUsername

--- src/main/java/com/app/encuestabackend/controllers/UserController.java
+++ src/main/java/com/app/encuestabackend/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.app.encuestabackend.controllers;
 
+import com.app.encuestabackend.models.requests.UserRegisterRequestModel;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     
     @PostMapping()
-    public String  createUser(){
+    public String  createUser(@RequestBody UserRegisterRequestModel userModel){
         return "Create user 2";
     }
     
--- src/main/java/com/app/encuestabackend/models/requests/UserRegisterRequestModel.java
+++ src/main/java/com/app/encuestabackend/models/requests/UserRegisterRequestModel.java
@@ -2,12 +2,21 @@ package com.app.encuestabackend.models.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserRegisterRequestModel {
 
+    @NotEmpty
     private String name;
 
-    private String subname;
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min= 8, max=40)
     private String password;
 }
